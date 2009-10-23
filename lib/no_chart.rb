fcb5ce090922684ff078731710dc1eea852b635a
cziko <niznik.pawel@gmail.com>
2009-10-23 23:35:17 +0200

    adding unobtrusive js UI for bars

--- lib/no_chart.rb
+++ lib/no_chart.rb
@@ -18,9 +18,8 @@ module NoChart
           elsif y == y_max
             template += "<td class='not_used'></td>"
           else
-            # TODO: need to introduce callbacks here!!!
             if data_array[x-2].to_f >= label_y[y_max-y-1].to_f
-              template += "<td class='used column_#{x-1}' onMouseOver=\"$('no_chart').select('td.column_#{x-1}').each(function(item){item.addClassName('selected')});\" onMouseOut=\"$('no_chart').select('td.column_#{x-1}').each(function(item){item.removeClassName('selected')});\"></td>"
+              template += "<td class='used column_#{x-1}'></td>"
             else
               template += "<td class='clear'></td>"
             end
